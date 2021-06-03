46468b2a57ce2b15dc00b2c9dd7d56e356d935d3
Matheus Machado de Almeida <40668494+MatheusMachado57@users.noreply.github.com>
2021-06-03 07:49:52 -0300

    Atualzando

--- Tarefa Aplicada 2.R
+++ Tarefa Aplicada 2.R
@@ -18,38 +18,49 @@ q2 = function(n,r){
 }
 q2(9,6)
 
-# Q3 - 10000 replica??es de um PA de 199 passos;
+# Q3 - 10000 replicacoes de um PA de 199 passos;
 #      guardar: A) n de troca de sinais
-#               B) o m?x atingido por cada passeio
+#               B) o max atingido por cada passeio
 #               C) n de lados acima do eixo
 
 q3 = function(n,r){
      f  = c(NULL)
      c  = c(NULL)
      b  = c(NULL)
-     for(i in 1:r){     # rodando os 1000 passeios
-         a  = 0         # zerando para a contagem do pr?ximo Passeio
-         d  = 0         # zerando para a contagem do pr?ximo Passeio
+     for(i in 1:r){     
+     # rodando os 1000 passeios
+         a  = 0         
+         # zerando para a contagem do proximo Passeio
+         d  = 0         
+         # zerando para a contagem do proximo Passeio
          v  = c(NULL)
          Sn = 0 
-     for(j in 1:n){    # rodando Passeio de 199 passos
-         m = runif(1)          # criando o passeio
+     for(j in 1:n){    
+     # rodando Passeio de 199 passos
+         m = runif(1)          
+         # criando o passeio
          if(m < 0.5){Sn = Sn + 1}else{Sn = Sn - 1}
             v[j] = Sn
-         if(j > 2){                                                           # um Passeio s? pode mudar de sinal depois do 2 momento;
-            if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1} # contando o n?mero de troca de sinal de cada passeio;  
+         if(j > 2){                                                           
+         # um Passeio so pode mudar de sinal depois do 2 momento;
+            if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1} 
+            # contando o numero de troca de sinal de cada passeio;  
          }
-         if(Sn > 0){d = d + 1}     # contando o n?mero de lados acima do eixo x de cada passeio
+         if(Sn > 0){d = d + 1}     
+         # contando o numero de lados acima do eixo x de cada passeio
      }                                
-       f[i] = a                 # armazenando o n?mero de troca de sinal de cada passeio         
-       b[i] = max(v)            # armazenando o maior valor de cada passeio 
-       c[i] = d                 # armazenando o n?mero de lados acima do eixo x   
+       f[i] = a                 
+       # armazenando o numero de troca de sinal de cada passeio         
+       b[i] = max(v)            
+       # armazenando o maior valor de cada passeio 
+       c[i] = d                 
+       # armazenando o numero de lados acima do eixo x   
      }
-     cat("\nN?mero de troca de sinais de cada Passeio:",f)
+     cat("\nNumero de troca de sinais de cada Passeio:",f)
      cat("\n")
      cat("\nO maior valor de cada Passeio:",b)
      cat("\n")
-     cat("\nO n?mero de lados de acima do eixo x de cada passeio:",c)
+     cat("\nO numero de lados de acima do eixo x de cada passeio:",c)
      plot(v)
 }
 
@@ -60,29 +71,42 @@ q3a = function(n,r){
   f  = c(NULL)
   c  = c(NULL)
   b  = c(NULL)
-  for(i in 1:r){    # rodando os 1000 passeios
-    a = 0           # zerando para a contagem do pr?ximo Passeio
-    d = 0           # zerando para a contagem do pr?ximo Passeio
-    v = c(NULL)     # limpando o vetor v para armazenar o pr?ximo Passeio
-    Sn = 0          # zerando para a contagem do Sn do pr?ximo Passeio
-    for(j in 1:n){          # rodando Passeio de 199 passos
-      m = runif(1)          # criando o passeio
+  for(i in 1:r){    
+    # rodando os 1000 passeios
+    a = 0           
+    # zerando para a contagem do proximo Passeio
+    d = 0           
+    # zerando para a contagem do proximo Passeio
+    v = c(NULL)     
+    # limpando o vetor v para armazenar o proximo Passeio
+    Sn = 0          
+    # zerando para a contagem do Sn do proximo Passeio
+    for(j in 1:n){          
+    # rodando Passeio de 199 passos
+      m = runif(1)          
+      # criando o passeio
       if(m < 0.5){Sn = Sn + 1}else{Sn = Sn - 1}
       v[j] = Sn
-      if(j > 2){                                                           # um Passeio s? pode mudar de sinal depois do 2 momento;
-        if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1}  # contando o n?mero de troca de sinal de cada passeio;  
+      if(j > 2){                                                           
+      # um Passeio so pode mudar de sinal depois do 2 momento;
+        if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1}  
+        # contando o numero de troca de sinal de cada passeio;  
       }
-      if(Sn > 0){d = d + 1}     # contando o n?mero de lados acima do eixo x de cada passeio
+      if(Sn > 0){d = d + 1}     
+      # contando o numero de lados acima do eixo x de cada passeio
     }                                
-    f[i] = a                    # armazenando o n?mero de troca de sinal de cada passeio         
-    b[i] = max(v)               # armazenando o maior valor de cada passeio 
-    c[i] = d                    # armazenando o n?mero de lados acima do eixo x   
+    f[i] = a                    
+    # armazenando o n?mero de troca de sinal de cada passeio         
+    b[i] = max(v)               
+    # armazenando o maior valor de cada passeio 
+    c[i] = d                    
+    # armazenando o n?mero de lados acima do eixo x   
   }
-  cat("\nN?mero de troca de sinais de cada Passeio:",f)
+  cat("\nNumero de troca de sinais de cada Passeio:",f)
   cat("\n")
   cat("\nO maior valor de cada Passeio:",b)
   cat("\n")
-  cat("\nO n?mero de lados de acima do eixo x de cada passeio:",c)
+  cat("\nO numero de lados de acima do eixo x de cada passeio:",c)
   hist(f)
 }
 
@@ -101,29 +125,42 @@ q3c = function(n,r){
   f  = c(NULL)
   c  = c(NULL)
   b  = c(NULL)
-  for(i in 1:r){    # rodando os 1000 passeios
-    a = 0           # zerando para a contagem do pr?ximo Passeio
-    d = 0           # zerando para a contagem do pr?ximo Passeio
-    v = c(NULL)     # limpando o vetor v para armazenar o pr?ximo Passeio
-    Sn = 0          # zerando para a contagem do Sn do pr?ximo Passeio
-    for(j in 1:n){          # rodando Passeio de 199 passos
-      m = runif(1)          # criando o passeio
+  for(i in 1:r){    
+  # rodando os 1000 passeios
+    a = 0           
+    # zerando para a contagem do proximo Passeio
+    d = 0           
+    # zerando para a contagem do proximo Passeio
+    v = c(NULL)     
+    # limpando o vetor v para armazenar o proximo Passeio
+    Sn = 0          
+    # zerando para a contagem do Sn do proximo Passeio
+    for(j in 1:n){          
+    # rodando Passeio de 199 passos
+      m = runif(1)          
+      # criando o passeio
       if(m < 0.5){Sn = Sn + 1}else{Sn = Sn - 1}
       v[j] = Sn
-      if(j > 2){                                                           # um Passeio s? pode mudar de sinal depois do 2 momento;
-        if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1}  # contando o n?mero de troca de sinal de cada passeio;  
+      if(j > 2){                                                           
+      # um Passeio so pode mudar de sinal depois do 2 momento;
+        if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1}  
+        # contando o numero de troca de sinal de cada passeio;  
       }
-      if(Sn > 0){d = d + 1}     # contando o n?mero de lados acima do eixo x de cada passeio
+      if(Sn > 0){d = d + 1}     
+      # contando o numero de lados acima do eixo x de cada passeio
     }                                
-    f[i] = a                    # armazenando o n?mero de troca de sinal de cada passeio         
-    b[i] = max(v)               # armazenando o maior valor de cada passeio 
-    c[i] = d                    # armazenando o n?mero de lados acima do eixo x   
+    f[i] = a                    
+    # armazenando o numero de troca de sinal de cada passeio         
+    b[i] = max(v)               
+    # armazenando o maior valor de cada passeio 
+    c[i] = d                    
+    # armazenando o n?mero de lados acima do eixo x   
   }
-  cat("\nN?mero de troca de sinais de cada Passeio:",f)
+  cat("\nNumero de troca de sinais de cada Passeio:",f)
   cat("\n")
   cat("\nO maior valor de cada Passeio:",b)
   cat("\n")
-  cat("\nO n?mero de lados de acima do eixo x de cada passeio:",c)
+  cat("\nO numero de lados de acima do eixo x de cada passeio:",c)
   hist(b)
 }
 q3c(199,1000)
@@ -139,35 +176,48 @@ q3e = function(n,r){
   f  = c(NULL)
   c  = c(NULL)
   b  = c(NULL)
-  for(i in 1:r){    # rodando os 1000 passeios
-    a = 0           # zerando para a contagem do pr?ximo Passeio
-    d = 0           # zerando para a contagem do pr?ximo Passeio
-    v = c(NULL)     # limpando o vetor v para armazenar o pr?ximo Passeio
-    Sn = 0          # zerando para a contagem do Sn do pr?ximo Passeio
-    for(j in 1:n){          # rodando Passeio de 199 passos
-      m = runif(1)          # criando o passeio
+  for(i in 1:r){    
+  # rodando os 1000 passeios
+    a = 0           
+    # zerando para a contagem do proximo Passeio
+    d = 0           
+    # zerando para a contagem do proximo Passeio
+    v = c(NULL)     
+    # limpando o vetor v para armazenar o proximo Passeio
+    Sn = 0          
+    # zerando para a contagem do Sn do proximo Passeio
+    for(j in 1:n){          
+    # rodando Passeio de 199 passos
+      m = runif(1)          
+      # criando o passeio
       if(m < 0.5){Sn = Sn + 1}else{Sn = Sn - 1}
       v[j] = Sn
-      if(j > 2){                                                           # um Passeio s? pode mudar de sinal depois do 2 momento;
-        if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1}  # contando o n?mero de troca de sinal de cada passeio;  
+      if(j > 2){                                                           
+      # um Passeio so pode mudar de sinal depois do 2 momento;
+        if(v[j - 2] > 0 & v[j] < 0 || v[j - 2] < 0 & v[j] > 0){a = a + 1}  
+        # contando o numero de troca de sinal de cada passeio;  
       }
-      if(Sn > 0){d = d + 1}     # contando o n?mero de lados acima do eixo x de cada passeio
+      if(Sn > 0){d = d + 1}     
+      # contando o numero de lados acima do eixo x de cada passeio
     }                                
-    f[i] = a                    # armazenando o n?mero de troca de sinal de cada passeio         
-    b[i] = max(v)               # armazenando o maior valor de cada passeio 
-    c[i] = d                    # armazenando o n?mero de lados acima do eixo x   
+    f[i] = a                    
+    # armazenando o numero de troca de sinal de cada passeio         
+    b[i] = max(v)               
+    # armazenando o maior valor de cada passeio 
+    c[i] = d                    
+    # armazenando o numero de lados acima do eixo x   
   }
-  cat("\nN?mero de troca de sinais de cada Passeio:",f)
+  cat("\nNumero de troca de sinais de cada Passeio:",f)
   cat("\n")
   cat("\nO maior valor de cada Passeio:",b)
   cat("\n")
-  cat("\nO n?mero de lados de acima do eixo x de cada passeio:",c)
+  cat("\nO numero de lados de acima do eixo x de cada passeio:",c)
   hist(c)
 }
 q3e(199,1000)
 
-# 4 - fun??o  q constr?i um P.A. em Z2, com n passos.
-#     A fu??o de ve retornar um gr?fico e o estado final; 
+# 4 - funcao  q constroi um P.A. em Z2, com n passos.
+#     A fucao de ve retornar um grafico e o estado final; 
 
 Q4 = function(n){
      x = 0
@@ -176,35 +226,45 @@ Q4 = function(n){
      z = c(NULL)
      v[1] = 0
      z[1] = 0
-     for(i in 2:n){                       # começa com 2. pois o primeiro momento já esta em (0,0)
+     for(i in 2:n){                       
+     # começa com 2. pois o primeiro momento já esta em (0,0)
          m = runif(1)
-         if(m<0.5){                       # se m < 0.5, se move na horizontal
+         if(m<0.5){                       
+         # se m < 0.5, se move na horizontal
             m=runif(1)                     
-            if(m<0.5){x=x+1}else{x=x-1}   # se m < 0.5, se move para direita, senão, para a esquerda 
+            if(m<0.5){x=x+1}else{x=x-1}   
+            # se m < 0.5, se move para direita, senão, para a esquerda 
             v[i] = x
-            if(i == 1){z[i] = 0}else      # a coordenada vertical se mantém
+            if(i == 1){z[i] = 0}else      
+            # a coordenada vertical se mantém
                       {z[i] = z[i-1]}
-            }else{                        # se m > 0.5, se move na vertical 
+            }else{                        
+            # se m > 0.5, se move na vertical 
             m=runif(1)  
-            if(m<0.5){y=y+1}else{y=y-1}   # se m < 0.5, se move para cima, senão, para baixo
+            if(m<0.5){y=y+1}else{y=y-1}   
+            # se m < 0.5, se move para cima, senão, para baixo
             z[i] = y
-            if(i == 1){v[i] = 0}else      # a coordenada horizontal se mantém
+            if(i == 1){v[i] = 0}else      
+            # a coordenada horizontal se mantém
                       {v[i] = v[i-1]}
      }}
-     plot(v,z, type = "l")                # plotando o gráfico em linhas
+     plot(v,z, type = "l")                
+     # plotando o gráfico em linhas
 }
 
 Q4(1000)
 
-# 5 - fun??o que contr?i n replica??es de um P.A. de n passos em Z2;
-#     retorna um hisograma com a dist?ncia de Sn a origem
+# 5 - funcao que controi n replicacoes de um P.A. de n passos em Z2;
+#     retorna um hisograma com a distancia de Sn a origem
+
 Q5 = function(n,r){
      x  = 0
      y  = 0
      v  = c(NULL)
      z  = c(NULL)
      Sn = c(NULL)
-     v[1]  = 0                      # similar ao passeio anterior
+     v[1]  = 0                      
+     # similar ao passeio anterior
      z[1]  = 0
      for(j in 1:r){
      for(i in 2:n){
@@ -228,23 +288,27 @@ Q5 = function(n,r){
 }
 Q5(1000,100)
 
-# 6 - graf. estrela at? repetir um viz
+# 6 - graf. estrela ate repetir um viz
 
 q6 = function(r,n){
-     m = c(NULL)     # vetor que conter? os valores de runif;
-     s = c(NULL)     # vetor que conter? o maior valor de cada instervalo;
+     m = c(NULL)     
+     # vetor que contera os valores de runif;
+     s = c(NULL)     
+     # vetor que contera o maior valor de cada instervalo;
      c = c(NULL)
      for (i in 1:r){
           s[i] = i/r
      }
      for(j in 1:n){
-     f = rep(0,r)     # vetor que conter? os intervalos ao qual cada runif pertence;    
+     f = rep(0,r)     
+     # vetor que contera os intervalos ao qual cada runif pertence;    
      for(i in 1:r){
          m[i] = runif(1)
          if(m[i]<0.5){f[i] = f[i] + 1}
          if(f[i] == 2)break}
      c[j] = sum(f)
-     }                      # obs: os intervalos s?o como os amigos e runif s?o como as visitas;
+     }                      
+     # obs: os intervalos sao como os amigos e runif sao como as visitas;
       hist(c)
       cat("\nVetor com o maior valor de cada intervalo:",s)
 #     cat("\nVetor com os vizinhos que foram visitados:",f)
@@ -257,17 +321,20 @@ q6(20,1000)
 q6(100,1000)
 
 
-# 7 - graf. estrela at? atingir tds os viz:
+# 7 - graf. estrela ata atingir tds os viz:
 
 q7 = function(r,n){
-     m = c(NULL)     # vetor que conter? os valores de runif;
-     s = c(NULL)     # vetor que conter? o maior valor de cada instervalo;
+     m = c(NULL)     
+     # vetor que contera os valores de runif;
+     s = c(NULL)     
+     # vetor que contera o maior valor de cada instervalo;
      c = c(NULL)
      for (i in 1:r){
           s[i] = i/r
      }
      for(j in 1:n){
-     f = rep(0,r)     # vetor que conter? os intervalos ao qual cada runif pertence;    
+     f = rep(0,r)     
+     # vetor que contera os intervalos ao qual cada runif pertence;    
      d = 1
      repeat{
          m[d] = runif(1)
@@ -276,7 +343,8 @@ q7 = function(r,n){
          d = d + 1
          if(sum(f) == r){break}}
      c[j] = sum(f)
-     }                      # obs: os intervalos s?o como os amigos e runif s?o como as visitas;
+     }                      
+     # obs: os intervalos sao como os amigos e runif sao como as visitas;
       hist(c)
       cat("\nVetor com o maior valor de cada intervalo:",s)
 #     cat("\nVetor com os vizinhos que foram visitados:",f)
@@ -289,17 +357,25 @@ q7(100,1000)
 
 # 8 - P.A. circular:
 q8 = function(r,n,p){
-     c = 1                               # começando no primeiro vizinho (vizinho 1)
-     f = rep(0,r)                        # vetor para guardar os vizinhos visitados
+     c = 1                               
+     # começando no primeiro vizinho (vizinho 1)
+     f = rep(0,r)                        
+     # vetor para guardar os vizinhos visitados
      for(i in 1:n){
          m = runif(1)
-         if(m<p){c = c + 1}else{c = c - 1}    # pulando de vizinho em vizinho
-         if(c > r){c = 1}                     # se maior que r, volta pro primeiro vizinho
-         if(c == 0){c = r}                    # se menor que 1, vai para o último vizinho
-         f[c] = f[c] + 1                      # avisando que tal vizinho foi vizitado
-         if(f[c] == 2){f[c] = 1}              # anulando as segundas visitas
+         if(m<p){c = c + 1}else{c = c - 1}    
+         # pulando de vizinho em vizinho
+         if(c > r){c = 1}                     
+         # se maior que r, volta pro primeiro vizinho
+         if(c == 0){c = r}                    
+         # se menor que 1, vai para o último vizinho
+         f[c] = f[c] + 1                      
+         # avisando que tal vizinho foi vizitado
+         if(f[c] == 2){f[c] = 1}              
+         # anulando as segundas visitas
      }
-     cat(sum(f))    # retornando o número de vizinhos visitados; (onde f[i] = 1)
+     cat(sum(f))    
+     # retornando o número de vizinhos visitados; (onde f[i] = 1)
 }
 
 q8(10,10,0.5)
@@ -310,10 +386,14 @@ q9 = function(i,r,p){
      j = 1
      repeat{
            m = runif(1)
-           if(m<p){i = i +1}else{i = i -1}   # se o jogador ganha ou perde
-           v[j] = i                          # armazenando a informação
-           j = j + 1                         # proseguindo com o passeio
-           if(i == 0 || i == r)break         # se atingir 0 ou r, o jogador para;
+           if(m<p){i = i +1}else{i = i -1}   
+           # se o jogador ganha ou perde
+           v[j] = i                          
+           # armazenando a informação
+           j = j + 1                         
+           # proseguindo com o passeio
+           if(i == 0 || i == r)break         
+           # se atingir 0 ou r, o jogador para;
            }
      cat("Sn:",i)
      plot(v)
@@ -327,7 +407,8 @@ q10 = function(i,r,p,n){
      w = c(NULL)            
      j = 1
      q = 1
-     for(g in 1:n){                  # similar ao código anterior, porém com n passeios aleatórios
+     for(g in 1:n){                  
+     # similar ao código anterior, porém com n passeios aleatórios
      t = 0   
      repeat{                                  
            m = runif(1)
